d9129ab0665705b00ee53a47b83d42ee129d532c
James Matchett <jamesc.matchett@gmail.com>
2015-03-08 21:52:04 -0400

    updates like crazy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,10 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require foundation
 //= require zeroclipboard
+//= require foundation
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
+
 
